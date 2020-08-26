dc87d072bb48a7834ce09ca92b1750395a1db35f
Dalia Gadelhak <daliagadelhak@gmail.com>
2020-08-26 12:34:33 +0200

    build

---
+++ precache-manifest.7dd9386b141b48cb3c3182bf3e4f3565.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6f7ad47543f811b8217e2d68c980150",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65348c625c69ccb2449c",
+    "url": "/static/css/main.44cc4b8c.chunk.css"
+  },
+  {
+    "revision": "bfb3efbd8d2e568dfaaa",
+    "url": "/static/js/2.0cc69af8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0cc69af8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65348c625c69ccb2449c",
+    "url": "/static/js/main.cbf95803.chunk.js"
+  },
+  {
+    "revision": "f8a6c8d2974cd5d7d100",
+    "url": "/static/js/runtime-main.ca5bd03b.js"
+  },
+  {
+    "revision": "b2f624ca7b26fb6bb5f5a15a9bd76372",
+    "url": "/static/media/cold.b2f624ca.jpg"
+  },
+  {
+    "revision": "e977004b30a8470c97aa111b94c7b85b",
+    "url": "/static/media/good2.e977004b.jpg"
+  },
+  {
+    "revision": "b7abac1c2011c691746817b9673524f8",
+    "url": "/static/media/warm.b7abac1c.jpg"
+  }
+]);
